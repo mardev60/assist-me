79423da2f73408a35083359fea523621a94ec702
devbu <emmanuelniasse@gmail.com>
2024-10-18 17:10:58 +0200

    fix: cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,23 @@
-import express from 'express';
-import { helloWorldController } from './helloWorld/helloWorldController';
-import dotenv from 'dotenv';
-import { loginController, meController, registerController } from './auth/authController';
-import { authenticateToken } from './auth/authMiddleware';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import {
+    loginController,
+    meController,
+    registerController,
+} from "./auth/authController";
+import { authenticateToken } from "./auth/authMiddleware";
+import { helloWorldController } from "./helloWorld/helloWorldController";
 
 dotenv.config();
 const app = express();
 const port = 3000;
+app.use(cors());
 app.use(express.json());
 
-app.get('/hello', helloWorldController);
-app.post('/login', loginController);
-app.post('/register', registerController);
-app.get('/me', authenticateToken, meController);
+app.get("/hello", helloWorldController);
+app.post("/login", loginController);
+app.post("/register", registerController);
+app.get("/me", authenticateToken, meController);
 
 app.listen(port, () => console.log(`> Listening on port ${port}`));
